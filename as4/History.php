150e10c978dddf72f4997cecba2eff122669d22d
Jasonzjzhou <zz@myscc.ca>
2021-10-20 16:44:32 -0400

    added a div, changed id

--- as4/History.php
+++ as4/History.php
@@ -11,16 +11,16 @@
            echo "<h1>The History of Golden Retriever</h1>"
            echo "<div id='container'>";
                 require_once("includes/header.php");
-                echo "<div><img src='img/Skeleton2.jpg' class='image'></div><div id='col1'>
+                echo "<div><img src='img/Skeleton2.jpg' class='image'></div><div id='col4'>
                     <h2>From United Kindom</h2>
                     <p>Golden Retrievers were firstly registered by The Kennel Club in 1903. They were first exhibited in 1908, 
                     and in 1911 Golden Retrievers were recognized as a breed (Golden and Yellow Retrievers).</p>
-                </div><div id='col2'>
+                </div><div id='col4'>
                     <h2>From United States</h2>
                     <p>in 1925, the American Kennel Club started breed Golden Retrievers in 1925. Following that, the Golden Retriever Club of 
                     America was founded in 1938. As of the year 1999, 62,652 have been registered and ranked #2, and the only breed above 
                     them is the Labrador Retriever. </p>
-                </div>";
+                </div><div id='col4'></div>";
             require_once("includes/footer.php");
             echo "</div>";         
         ?>
