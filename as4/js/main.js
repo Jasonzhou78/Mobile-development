db0a53a103636f0759129b83be7dd09bf0278b36
Jasonzjzhou <zz@myscc.ca>
2021-10-25 14:58:51 -0400

    Update main.js

--- as4/js/main.js
+++ as4/js/main.js
@@ -1,6 +1,27 @@
 $(function() {
-    // add a click event at the element with id = hamburger.
-    $("#hamburger").click(function() {
-        $(this).show();
+    $(".hmenu").click(function() {
+        $("#header").slideToggle();
     });
+
+    $("#col6").click(function() {
+        $(this).fadeOut();
+    });
+
+    $("#container").delegate("#col4", "click", function() {
+        $(this).css("background-color", "pink");
+    });
+
+    $("#container").delegate("#col5", "click", function() {
+        $(this).fadeToggle();
+    });
+
+    $("#container").delegate("#col3", "click", function() {
+        $(this).fadeTo(1000, 0.5);
+    });
+
+    $("#container").delegate("p", "click", function() {
+        $(this).css("color", "red");
+    });
+
+
 });
