c1d89f0c777edb8be2a8bbd2a345665e63af195a
Jasonzjzhou <zz@myscc.ca>
2021-10-25 14:57:46 -0400

    Update as3.php

--- as4/as3.php
+++ as4/as3.php
@@ -8,18 +8,31 @@ Topic: Responsive web page
 	<head>
 		<title>St. Clair College</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/main.css" type="text/css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display&display=swap" rel="stylesheet">
 	</head>
 	<body>
 	<?php	
-
 		echo "<div id='container'>";
+		echo "<h1>Golden Retriever Introduction</h1>";
         require_once("includes/header.php");
-		echo "<h1>About Golden Retriever</h1>";
-		echo "<div><img src='img/img.jpg' class='image'></div>".
-		"<div id='col6'></div><div id='col3'></div><div id='col3'></div>";
+		echo "<div><img src='img/img.jpg' class='image'></div>
+        <div id='col5'><h2>From United States</h2>
+		<p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p></div><div id='col3'><h2>From United States</h2>
+		<p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p></div><div id='col4'><h2>From United States</h2>
+		<p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p></div><div id='col3'><h2>From United States</h2>
+		<p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p></div><div id='col5'><h2>From United States</h2>
+		<p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p></div><div id='col2'><h2>From United States</h2>
+		<p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p></div><div id='col2'><h2>From United States</h2>
+		<p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p></div>";
+        echo "</div>";
+
         require_once("includes/footer.php");
 		echo "</div>";
     ?>
+		<script src="js/jquery.js"></script>
+        <script src="js/main.js" defer></script>
 	</body>
 </html>
