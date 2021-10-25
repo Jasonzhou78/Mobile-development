f79a01c9738b5e2818097781725bf2916d661cec
Jasonzjzhou <zz@myscc.ca>
2021-10-25 14:56:58 -0400

    Update Characteristics.php

--- as4/Characteristics.php
+++ as4/Characteristics.php
@@ -4,24 +4,35 @@
 		<title>St. Clair College</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="css/main.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display&display=swap" rel="stylesheet">
 	</head>
 	<body>
         <?php
-
             echo "<div id='container'>";
+            echo "<h1>The Characteristics of Golden Retriever</h1>";
                 require_once("includes/header.php");
-                echo "<h1>The Characteristics of Golden Retriever</h1>";
                 echo "<div><img src='img/temper.jpg' class='image'></div>
                 <div id='col3'>
                     <h2>Coat</h2>
-                    <p class='paragraph'>As indicated by their name, their coats occur in light golden to dark golden colours.But some 
-                        may be light white. Golden Retrievers is medium-large, strongly built breed. </p>
-                </div><div id='col3'>
+                    <p class='paragraph'>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+                </div><div id='col4'>
                     <h2>Temperament</h2>
-                    <p class='paragraph'>The temperament of the Golden Retriever is a hallmark of the breed, and is described in the standard as kindly, friendly and confident.</p>
-                </div><div id='col6'></div>";
+                    <p class='paragraph'>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+                </div><div id='col5'>
+                    <h2>Color</h2>
+                    <p class='paragraph'>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p></div>";
+            echo "<div id='col4'><h2>From United States</h2>
+            <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p></div><div id='col3'><h2>From United States</h2>
+            <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p></div><div id='col2'><h2>From United States</h2>
+            <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p></div><div id='col2'><h2>From United States</h2>
+            <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p></div><div id='col1'><h2>From United States</h2>
+            <p>Lorem ipsum is placeholder text commonly used in the graphic.</p></div>";
                 require_once("includes/footer.php");
 		    echo "</div>";
         ?>
+        <script src="js/jquery.js"></script>
+        <script src="js/main.js" defer></script>
 	</body>
 </html>
