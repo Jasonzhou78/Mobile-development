2237899b96da1e32af14740769a64fd45231c782
Jasonzjzhou <zz@myscc.ca>
2021-10-29 08:18:10 -0400

    Update contact_us.php

--- as5/contact_us.php
+++ as5/contact_us.php
@@ -12,6 +12,14 @@ Topic: Responsive web page
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display&display=swap" rel="stylesheet">
+		<style>
+            #googleMap { width: 100%; height: 400px; }
+            
+            @media all and (max-width: 639px) {
+                #googleMap { width: 100%; height: 600px; }                
+            }
+        </style>
+		<script src="https://maps.google.com/maps/api/js?key=AIzaSyBkGyX_vgGgWplpkeot4vZUffY-BwfMghU"></script>	
 	</head>
 	<body>
 	<script>
@@ -30,7 +38,57 @@ Topic: Responsive web page
 			
 		    document.getElementById("currentLocation").innerHTML = "Latitude: " + lat + 
 		    "<br>Longitude: " + lon;
+			document.getElementById("directions").innerHTML = "<a href='"+dir_url+"'>GET DIRECTIONS TO THE CN TOWER</a>";
+			showMap(position); 
 		}
+		function showMap(position) {
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+        var zoom = 12;
+        
+        if (window.innerWidth > 639) {
+            zoom = 3;
+        } else {
+            zoom = 18;
+        }
+        
+        const myCenter = { lat: lat, lng: lon };
+        
+        var mapProperties = {
+            center:myCenter,
+            zoom:zoom,
+        };
+        var map = new google.maps.Map(document.getElementById("googleMap"),mapProperties);
+        
+        var marker = new google.maps.Marker({position: myCenter});
+        marker.setMap(map);    
+        
+        google.maps.event.addDomListener(window, "resize", function() {
+            if (window.innerWidth > 639) {
+                map.setZoom(12);
+            } else {
+                map.setZoom(18);
+            }         
+        });
+    }
+	
+	function showError(error) {
+    	var myDiv = document.getElementById("location");
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+                myDiv.innerHTML = "User denied the request for Geolocation."
+                break;
+            case error.POSITION_UNAVAILABLE:
+                myDiv.innerHTML = "Location information is unavailable."
+                break;
+            case error.TIMEOUT:
+                myDiv.innerHTML = "The request to get user location timed out."
+                break;
+            case error.UNKNOWN_ERROR:
+                myDiv.innerHTML = "An unknown error occurred."
+                break;
+        }
+    }
 	</script>
 		<?php
 		echo "<div id='container'>";
