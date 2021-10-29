33b0f56e9c248c3688d6e4208b3a2950cd8c688e
Jasonzjzhou <zz@myscc.ca>
2021-10-29 16:22:36 -0400

    Update contact.php

--- as5/contact.php
+++ as5/contact.php
@@ -1,4 +1,12 @@
 
-<?php
-    
-?>
+		<?php
+			$lastName = $_POST['lname'];
+			$firstName = $_POST['fname'];
+			$email = $_POST['email'];
+			$phoneNumber = $_POST['phone'];
+			$submit = $_POST['submit'];
+			if(isset($submit)) {
+				echo "<div> You have successfully submitted your contact information!</div>";
+				echo "<div><a href='contact_us.php'></a>Back</div>";
+			}
+		?>
