bd77c43b4b298d70d0b99f08ea4dcab46c41fb53
Jasonzjzhou <zz@myscc.ca>
2021-10-28 23:40:45 -0400

    Update contact_us.php

--- as5/contact_us.php
+++ as5/contact_us.php
@@ -13,36 +13,31 @@ Topic: Responsive web page
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display&display=swap" rel="stylesheet">
 	</head>
+	<body>
+	<script>
+		function getLocation() {
+		    if (navigator.geolocation) {
+		        navigator.geolocation.getCurrentPosition(showPosition);
+		    } else { 
+		        document.getElementById("currentLocation").innerHTML = "Geolocation is not supported by this browser.";
+		    }
+		}
 	
-			<script>
-			function getLocation() {
-				if (navigator.geolocation) {
-					navigator.geolocation.getCurrentPosition(showPosition);
-				} else { 
-					document.getElementById("currentLocation").innerHTML = "Geolocation is not supported by this browser.";
-				}
-			}
-		
-			function showPosition(position) {	
-				
-				var lat = position.coords.latitude;
-				var lon = position.coords.longitude;		
-				
-				document.getElementById("currentLocation").innerHTML = "Latitude: " + lat + 
-				"<br>Longitude: " + lon;
-			}
-		</script>
-		<body>
+		function showPosition(position) {	
+			
+		    var lat = position.coords.latitude;
+			var lon = position.coords.longitude;		
+			
+		    document.getElementById("currentLocation").innerHTML = "Latitude: " + lat + 
+		    "<br>Longitude: " + lon;
+		}
+	</script>
 		<?php
-			echo "<div class='location'>";
-				<p>Click the button to get your coordinates.</p>
-				<button onclick="getLocation();">Show My Location</button>	
-				<div id="currentLocation"></div>
-			echo "</div>";
-
-			echo "<div>";
-        	require_once("includes/footer.php");
-			echo "</div>";
+		echo "<div>";
+		echo "<p>Click the button to get your coordinates.</p>
+		<button onclick='getLocation();'>Show My Location</button>";
+		echo "</div>";
+		echo "<div id='currentLocation'></div>";
 		?>
 		<script src="js/jquery.js"></script>
         <script src="js/main.js" defer></script>
