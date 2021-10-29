6abf9086639acfa74d1c0c27d12f3560f05e05c0
Jasonzjzhou <zz@myscc.ca>
2021-10-28 23:45:19 -0400

    Update contact_us.php

--- as5/contact_us.php
+++ as5/contact_us.php
@@ -8,7 +8,7 @@ Topic: Responsive web page
 	<head>
 		<title>St. Clair College</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="css/main.css" type="text/css">
+		<link rel="stylesheet" href="./css/main.css" type="text/css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display&display=swap" rel="stylesheet">
